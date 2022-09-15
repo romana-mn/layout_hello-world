8624982c4900846677270ad58104bed304c99a54
Strybog <mych2207@gmail.com>
2022-09-15 21:29:41 +0300

    make build

---
+++ report/html_report/config.js
@@ -0,0 +1,32 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Entire_document.png",
+        "test": "..\\bitmaps_test\\20220915-212736\\Entire_document.png",
+        "selector": "document",
+        "fileName": "Entire_document.png",
+        "label": "Entire document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/hello-world/",
+        "expect": 0,
+        "viewportLabel": "custom-size",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -30
+          },
+          "rawMisMatchPercentage": 14.2125,
+          "misMatchPercentage": "14.21",
+          "analysisTime": 18
+        },
+        "diffImage": "..\\bitmaps_test\\20220915-212736\\failed_diff_Entire_document.png"
+      },
+      "status": "fail"
+    }
+  ]
+});
